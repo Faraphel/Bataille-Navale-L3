b9e087d1fdcfca5331b8e68605fa0af0db7f5a2c
Faraphel <rc60650@hotmail.com>
2023-02-20 22:23:57 +0100

    fixed a bug where the x and y where inverted in the board class

--- source/utils/copy_array_offset.py
+++ source/utils/copy_array_offset.py
@@ -10,6 +10,6 @@ def copy_array_offset(src: np.array, dst: np.array, offset: Point2D) -> None:
     :dst: destination array
     :offset: the offset where to copy the array
     """
-    row, column = offset
+    column, row = offset
     width, height = src.shape
     dst[row:row + width, column:column + height] = src
